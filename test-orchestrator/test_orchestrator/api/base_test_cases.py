79b1fd1d7ac7d4b9ddd1d1f636930dedd6e2c9b4
Mathias Moser <mathias.moser@catena-x.net>
2025-06-11 14:07:53 +0200

    feat: solved problem with test-orchestrator

--- test-orchestrator/test_orchestrator/api/base_test_cases.py
+++ test-orchestrator/test_orchestrator/api/base_test_cases.py
@@ -113,7 +113,7 @@ async def dtr_ping_test(counter_party_address: str,
                 limit=1)
 
         shell_descriptors = await make_request('GET',
-                                               f'{config.DT_PULL_SERVICE_ADDRESS}/dtr/shell-descriptors',
+                                               f'{config.DT_PULL_SERVICE_ADDRESS}/dtr/shell-descriptors/',
                                                params={'dataplane_url': dataplane_url},
                                                headers = get_dt_pull_service_headers(headers={'Authorization': dtr_key})
                                                )
